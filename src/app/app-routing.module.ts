63f6a0b93cd065a695292dbb23d0d464dda1bc1c
Hélio Luna <helioluna@outlook.com>
2020-02-12 22:26:07 -0300

    Initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,22 @@
+import { PedidoComponent } from './pedido/pedido.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  //{ path: 'pedido/:id',      component: HeroDetailComponent },
+  {
+    path: 'pedido',
+    component: PedidoComponent,
+    data: { title: 'Pedido' }
+  },
+  { path: '',
+    redirectTo: '/pedido',
+    pathMatch: 'full'
+  },
+//{ path: '**', component: PageNotFoundComponent }
+];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
